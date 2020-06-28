af5629ab9e7509992f3bedb276a59731c89ef6aa
Leon B. Haase <leon.haase@gmx.net>
2020-06-28 16:06:23 +0200

    Fix GetAllListsOfAUser

--- public/verarbeiten.php
+++ public/verarbeiten.php
@@ -635,9 +635,7 @@
       $stmt = $mysqli->query($sql);
 
       for ($x = 0; $row = mysqli_fetch_assoc($stmt); $x++) {
-        $ergebnis[$x] =  array("id" => $row["id"]);
-        $ergebnis[$x] =  array("name" => $row["name"]);
-        $ergebnis[$x] =  array("creator" => $row["creator"]);
+        $ergebnis[$x] =  array("id" => $row["id"], "name" => $row["name"], "creator" => $row["creator"]);
       }
       return $ergebnis;
     } else {

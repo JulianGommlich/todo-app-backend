9e3b1c82981a47d893d84832850f223d8a6292ed
Leon B. Haase <leon.haase@gmx.net>
2020-06-29 17:44:37 +0200

    Fix?

--- public/index.php
+++ public/index.php
@@ -203,10 +203,5 @@ $app->delete('/api/lists', function (Request $request, Response $response, $args
     return ($del != null) ? $response : $response->withStatus(401);
 });
 
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
-    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-    return $handler($req, $res);
-});
-
 $app->run();
 ?>
